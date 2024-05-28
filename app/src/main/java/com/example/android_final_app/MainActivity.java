492b92f3394b82029c2ee66759a218c62af8d555
taegyeong0225 <202244042@itc.ac.kr>
2024-05-28 15:48:26 +0900

    하단 네비게이션 구현

--- app/src/main/java/com/example/android_final_app/MainActivity.java
+++ app/src/main/java/com/example/android_final_app/MainActivity.java
@@ -11,9 +11,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-
-    private Button loginTest;
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,28 +19,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-        loginTest = (Button)findViewById(R.id.btnLoginTest);
-        loginTest.setOnClickListener(this);
-
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
     }
-
-    // 클릭시 발생하는 이벤트
-    @Override
-    public void onClick(View v) {
-        // 로그인 버튼 클릭시 로그인 액티비티로 이동
-        if(v == loginTest) {
-            Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
-            startActivity(loginIntent);
-        }
-//        else if (v == [버튼명]){
-//            Intent [인텐트명] = new Intent(MainActivity.this, [액티비티명].class);
-//            startActivityForResult([인텐트명], 1);
-//        }
-    }
-
 }
